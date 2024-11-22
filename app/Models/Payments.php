37cc21572ed8e43b9c68e12f61ee617bc5eb2e2e
Hasyimmuarifin <hasyimviic@gmail.com>
2024-11-22 21:12:46 +0700

    feat: Method POST Payments v1

--- app/Models/Payments.php
+++ app/Models/Payments.php
@@ -24,4 +24,9 @@ class Payments extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function booking()
+    {
+        return $this->hasOne(Booking::class);
+    }
 }
