b90dca95d268b44fed2c32938bc90d19e130098a
puspanana <puspadenana@gmail.com>
2024-11-24 20:52:28 +0700

    feat: Method PUT Payments Update

--- app/Http/Controllers/Api/PaymentsController.php
+++ app/Http/Controllers/Api/PaymentsController.php
@@ -102,9 +102,54 @@ class PaymentsController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Payments $payments)
+    public function update(Request $request,  $id)
     {
-        //
+        try{
+            $payments = Payments::findOrFail($id);
+            $rules = [
+                'user_id' => 'required',
+                'booking_id' => 'required',
+                'total_payment' => 'required',
+                'payment_method' => 'required',
+                'status' => 'required',
+                'order_id' => 'required|numeric|min:0',
+                'receipt' => 'required|numeric|min:0',
+                'date' => 'required|date',
+
+            ];
+            $validator = validator::make($request->all(),$rules);
+            if($validator->fails()){
+                return response()->json([
+                    'success' =>false,
+                    'message'=>'Validation errors',
+                    'data' => $validator->errors(),
+                ],422);
+            }
+            $payments->user_id = $request->user_id;
+            $payments->booking_id = $request->booking_id;
+            $payments->total_payment = $request->total_payment;
+            $payments->payment_method = $request->payment_method;
+            $payments->status = $request->status;
+            $payments->order_id = $request->order_id;
+            $payments->receipt = $request->receipt;
+            $payments->date = $request->date;
+
+            $payments->save();
+            return response()->json([
+                'success' => true,
+                'message' => 'Payment updated successfully',
+                'data' => $payments,
+            ], 200);
+                
+        }catch(\Exception $e){
+            return response()->json([
+                'success' =>false,
+                'message' => 'Failed to update payment',
+                'error' => $e->getMessage(),
+            ],500);
+        }
+    
+        
     }
 
     /**
